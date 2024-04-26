b26717ab26ee58dbd0e8d87b86698a713ec0d721
varunvaatsalya <a.varungupta23@gmail.com>
2024-04-26 12:15:08 +0530

    add admin section & blogs

---
+++ src/app/api/getUpdateData/route.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+import { NextResponse } from "next/server";
+
+export function GET(Request) {
+  let Data = fs.readFileSync("src/app/data2.json");
+  Data = JSON.parse(Data);
+  return NextResponse.json(Data, { status: 200 });
+}
+
+export async function POST(Request) {
+  let payload = await Request.json();
+  const updatedJsonData = JSON.stringify(payload);
+  fs.writeFile("src/app/data2.json", updatedJsonData, function (err) {
+    if (err) console.log(err);
+  });
+
+  return NextResponse.json({ message: "success" }, { status: 200 });
+}
